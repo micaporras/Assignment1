c9ae39aba12627e5bd30e99ff506a4a8f959bfde
Mica Lorraine D. Porras <92613141+micaporras@users.noreply.github.com>
2021-10-23 13:32:42 +0800

    Delete program1.py

--- program1.py
+++
@@ -1,4 +0,0 @@
-name = input('What is your name?:')
-age = input('How old are you?:')
-address = input('Where do you live?:')
-print(f'Hi, my name is {name}. I am {age} years old and I live in {address}')
